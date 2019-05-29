55ae96180e73533ff0bf9690f62968fa863ccdf8
Izumi Matsuda <izumimatsuda@Izumis-MacBook-Pro.local>
2019-05-28 17:50:27 -0700

    Created new Vuforia class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Spin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Spin.java
@@ -1,6 +1,6 @@
+
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,31 +8,29 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@Autonomous(name="BallAuto", group="Iterative Opmode")
-
-public class BallAuto extends OpMode
+@TeleOp(name="Spin", group="Iterative Opmode")
+public class Spin extends OpMode
 {
-    // Declare Variables
-    double leftPower;
-    double rightPower;
-    double position;
-
+    // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
 
-    Vuforia vu;
-
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
     @Override
     public void init() {
-        //Code to run ONCE when the driver hits INIT
         telemetry.addData("Status", "Initialized");
 
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
-        vu = new Vuforia(hardwareMap);
-
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -44,42 +42,48 @@ public class BallAuto extends OpMode
         telemetry.addData("Status", "Initialized");
     }
 
-
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
     @Override
     public void init_loop() {
-        //Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
     }
 
-
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
     @Override
     public void start() {
-        //Code to run ONCE when the driver hits PLAY
         runtime.reset();
     }
 
-
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
     @Override
     public void loop() {
-        //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-        if (rightDrive.getCurrentPosition() < 3850){
-            leftPower = 1;
-            rightPower = 1;
-        }else{
-            leftPower = 0;
-            rightPower = 0;
-        }
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double leftPower;
+        double rightPower;
 
+        leftPower  = 1;
+        rightPower = -1;
+
+        // Send calculated power to wheels
         leftDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
 
+        // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-        telemetry.addData("Vuforia",vu.view());
+        telemetry.addData("EncoderTicks", rightDrive.getCurrentPosition());
     }
 
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
     @Override
     public void stop() {
-        //Code to run ONCE after the driver hits STOP
     }
 
 }
